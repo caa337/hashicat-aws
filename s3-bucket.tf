be76d7d7ee1e5ac336e63c4304b03cd428e08e17
root <root@p-tsifmxmw07cg-workstation.c.instruqt-prod.internal>
2021-02-10 19:25:25 +0000

    Added s3 bucket module tags

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,4 +2,8 @@ module "s3-bucket" {
   source  = "app.terraform.io/Caseyarbaugh-training/s3-bucket/aws"
   version = "1.15.0"
   bucket_prefix = "caseyarbaugh"
+  tags =  {
+      Department = "Tech"
+      Billable = "yes"
+  }
 }
